61c73983901600694f74d1a0064d1e43a3173a2a
leslieo23 <leslie.obour23@gmail.com>
2016-04-09 12:31:32 -0400

    inital commit

---
+++ student.js
@@ -0,0 +1,8 @@
+function student(name,gender,GPA,dententions,sleepingInClass,catchPhrase){
+	this.name=name
+	this.gender=gender
+	this.gpa=GPA
+	this.dententions=dententions
+	this.sleeping=sleepingInClass
+	this.catch=catchPhrase
+}
