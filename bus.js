b146726cf548f308683f2a35d65181f2a96548eb
leslieo23 <leslie.obour23@gmail.com>
2016-04-13 11:52:45 -0400

    intial commit

---
+++ bus.js
@@ -0,0 +1,8 @@
+var bus=function(studentsOnTheBus,driverName,color,gas,studentEntersBus,busChatter){
+	this.studentsOnTheBus=studentsOnTheBus
+	this.driverName=driverName
+	this.color=color
+	this.gas=has
+	this.studentEnterBus=studentEnterBus
+	this.busChatter=busChatter
+}
--- student.js
+++ student.js
@@ -1,8 +1,22 @@
-function student(name,gender,GPA,dententions,sleepingInClass,catchPhrase){
+var prompt=require('prompt')
+var bus=require('./bus.js')
+var fs=require('fs')
+
+var busInfo= new bus();
+prompt.start()
+prompt.get(['name','gender','detentions','gpa','sleep','catchPhrase'],function(err,result){
+	var stduent=new Student(result.name,result.detentions,result.gpa,result.sleep,result.catchPhrasefun)
+});
+function Student(name,gender,GPA,dententions,sleepingInClass,catchPhrase){
 	this.name=name
 	this.gender=gender
 	this.gpa=GPA
 	this.dententions=dententions
 	this.sleeping=sleepingInClass
 	this.catch=catchPhrase
-}
+	this.canStudentHaveFun=function(){
+		if((this.detentions<10)&&(this.gpa>2)){
+			console.log("You can have fun! ")
+		}		
+	};
+};
